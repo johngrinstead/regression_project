4fa90a0b416f213b3038bf2e9536c53b93a410db
John Grinstead <john.allen.grinstead@gmail.com>
2021-03-23 22:42:47 -0500

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# regression_project
+# Regression Project
+# by John Grinstead
 
-## This is the repository I will use for my Regression project
+### This is the repository I will use for my Regression project
+
+Goal: The ultimate goal of this project is to create a regression model that will predict the tax value of homes in the Southern California area. Utilizing exploration and feature engineering I will identify the most and least valuable predicting variables in an effort to maximize the accuracy of my model. To be successful the final model must have a lower residual than the mean and median baselines.
+
+------------------------------------------------------------------------------------------------------------------
+
+### Project Planning
+
+(https://trello.com/b/wR1FFqhq/johns-regression-project)
+
+To plan this project I utilized a trello board to help keep my thoughts organized and keep track of the tasks I still need to accomplish
+
+------------------------------------------------------------------------------------------------------------------
+### Data Dictionary
+
+------------------------------------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------------------------------------------
--- wrangle.py
+++ wrangle.py
@@ -26,7 +26,7 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
 ## single unit property values
 
 sql = '''
-select parcelid, calculatedfinishedsquarefeet as square_feet, bedroomcnt as bedrooms, bathroomcnt as bathrooms, yearbuilt, taxvaluedollarcnt as tax_value
+select parcelid, calculatedfinishedsquarefeet as square_feet, bedroomcnt as bedrooms, bathroomcnt as bathrooms, yearbuilt, fips, lotsizesquarefeet as lot_size, taxvaluedollarcnt as tax_value
 from properties_2017
 join predictions_2017 using(parcelid)
 where transactiondate between "2017-05-01" and "2017-08-31"
@@ -39,6 +39,51 @@ OR  unitcnt = 1;
 
 
 def wrangle_zillow():
+    '''
+    This function will acquire and prep the Zillow data for initial exploration and modeling
+    '''
+    data = pd.read_csv("zillow.csv")
+    
+    data['house_age'] = (2021 - data.yearbuilt)
+    
+    data = data.drop(columns = ['Unnamed: 0', 'yearbuilt'])
+    
+    ## remove outliers, filter out extreme values and remove 'homes' with no rooms
+
+    index3500 = data.loc[data['square_feet'] >= 3500].index
+    data.drop(index3500 ,  inplace=True)
+    
+    index_tax_value = data.loc[data['tax_value'] >= 1680000].index
+    data.drop(index_tax_value ,  inplace=True)
+    
+    index_lot_size = data.loc[data['lot_size'] >= 23778.0].index
+    data.drop(index_lot_size ,  inplace=True)
+    
+    index_bedrooms = data.loc[data['bedrooms'] == 0].index
+    data.drop(index_bedrooms , inplace=True)
+    
+    index_bathrooms = data.loc[data['bathrooms'] == 0].index
+    data.drop(index_bathrooms , inplace=True)
+    
+    data = data.set_index("parcelid")
+    
+    data = data.dropna()
+    # remove all NaN values
+    
+    # Remove decimal
+    data['bedrooms'] = data['bedrooms'].astype(int)
+    data['bathrooms'] = data['bathrooms'].astype(int)
+    data['fips'] = data['fips'].astype(int)
+    data['house_age'] = data['house_age'].astype(int)
+    
+    return data
+
+
+def wrangle_zillow_fe():
+    '''
+    This function acquires and prep's the data with certain features to be removed after 
+    they were found to not be useful predictors with feature engineering.
+    '''
     data = pd.read_csv("zillow.csv")
     
     data['house_age'] = (2021 - data.yearbuilt)
@@ -72,6 +117,8 @@ def wrangle_zillow():
     data['house_age'] = data['house_age'].astype(int)
     
     return data
+
+
 
 
 def split(df, stratify_by=None):
